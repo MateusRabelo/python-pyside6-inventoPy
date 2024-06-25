68cc0590beabf2262fccbe62a55e1069874291f8
Arkesdev <mateusaqles@gmail.com>
2024-06-25 19:20:12 -0300

    added a splash screen

--- environment/envVariables.py
+++ environment/envVariables.py
@@ -5,5 +5,7 @@ ROOT_DIR = (Path(__file__).parent.parent)
 RESOURCES_DIR = ROOT_DIR / 'resources'
 STYLE_DIR = RESOURCES_DIR / 'style'
 
-WINDOW_ICON_PATH = RESOURCES_DIR / 'images/icons/icon.png'
+WINDOW_ICON_PATH = RESOURCES_DIR / 'images/icons/app-icon.png'
 LOGIN_STYLESHEET_PATH = STYLE_DIR / 'loginStyle.qss'
+
+SPLASHSCREEN_PATH = RESOURCES_DIR / 'images/splash-image.png'
