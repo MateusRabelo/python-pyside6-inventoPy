3e48f9101253ea0d6886b7f38c75f33fc5e57cfd
Arkesdev <mateusaqles@gmail.com>
2024-06-25 21:18:37 -0300

    update: now the main.py is a SplashScreen class

--- environment/envVariables.py
+++ environment/envVariables.py
@@ -4,8 +4,11 @@ ROOT_DIR = (Path(__file__).parent.parent)
 
 RESOURCES_DIR = ROOT_DIR / 'resources'
 STYLE_DIR = RESOURCES_DIR / 'style'
+IMAGE_DIR = RESOURCES_DIR / 'images'
 
-WINDOW_ICON_PATH = RESOURCES_DIR / 'images/icons/app/app-icon.png'
+WINDOW_ICON_PATH = IMAGE_DIR / 'icons/app/app-icon.png'
 LOGIN_STYLESHEET_PATH = STYLE_DIR / 'loginStyle.qss'
 
-SPLASHSCREEN_PATH = RESOURCES_DIR / 'images/splash-image.png'
+SPLASHSCREEN_PATH = IMAGE_DIR / 'splash-image.png'
+
+LOGIN_IMAGE_PATH = IMAGE_DIR / 'login-image.png'
--- main.py
+++ main.py
@@ -1,6 +1,6 @@
 from PySide6.QtWidgets import QApplication, QSplashScreen, QLabel, QVBoxLayout, QHBoxLayout, QDialog
 from PySide6.QtGui import QPixmap
-from PySide6.QtCore import Qt
+from PySide6.QtCore import Qt, QPoint
 
 from app.views.authentication import LoginWindow
 from app.views.application import MainWindow
@@ -9,32 +9,48 @@ import time
 
 from environment.envVariables import SPLASHSCREEN_PATH
 
+class SplashScreen(QSplashScreen):
+    def __init__(self, parent=None):
+        super().__init__(parent)
+
+        self.initUI()
+
+    def initUI(self):
+        # Criar um layout horizontal para a splash screen
+        layout = QHBoxLayout(self)
+        self.setLayout(layout)
+
+        # Adicionar a imagem à splash screen
+        splashPix = QPixmap(SPLASHSCREEN_PATH)
+        imageLabel = QLabel()
+        imageLabel.setPixmap(splashPix.scaled(100, 100, Qt.AspectRatioMode.KeepAspectRatio))
+        layout.addWidget(imageLabel)
+
+        # Adicionar uma mensagem à splash screen
+        message = QLabel("Loading . . .")
+        message.setStyleSheet("font-size: 40px; color: white;")
+        layout.addWidget(message, Qt.AlignLeft | Qt.AlignBottom)
+
+        self.centerDialog()
+
+    def centerDialog(self):
+        screen = QApplication.primaryScreen()
+        screenGeometry = screen.availableGeometry()
+        windowGeometry = self.frameGeometry()
+        centerPoint = screenGeometry.center()
+        windowGeometry.moveCenter(centerPoint)
+        # offset = screenGeometry.topLeft() - windowGeometry.topLeft()
+        self.move(windowGeometry.topLeft() - QPoint(180, 100))
+
 if __name__ == "__main__":
     import sys
     app = QApplication(sys.argv)
 
-    # Criar a splash screen
-    splash_pix = QPixmap(SPLASHSCREEN_PATH)
-    splash = QSplashScreen(splash_pix, Qt.WindowStaysOnTopHint)
-
-    # Criar um layout horizontal para a splash screen
-    layout = QHBoxLayout(splash)
-    splash.setLayout(layout)
-
-    # Adicionar a imagem à splash screen
-    image_label = QLabel()
-    image_label.setPixmap(splash_pix.scaled(100, 100, Qt.AspectRatioMode.KeepAspectRatio))
-    layout.addWidget(image_label)
-
-    # Adicionar uma mensagem à splash screen
-    message = QLabel("Loading . . .")
-    message.setStyleSheet("font-size: 24px; color: white;")
-    layout.addWidget(message, Qt.AlignLeft | Qt.AlignBottom)
-
+    splash = SplashScreen()
     splash.show()
 
     # Simular um carregamento demorado
-    time.sleep(5)
+    time.sleep(2)
 
     # Fechar a splash screen
     splash.close()
@@ -44,8 +60,6 @@ if __name__ == "__main__":
     login_window.setWindowModality(Qt.ApplicationModal)
     if login_window.exec() == QDialog.Accepted:
         main_window = MainWindow()
-
         main_window.show()
         main_window.fixAllSize()
-        
         sys.exit(app.exec())
